bbd3203f84648d632b0faf1cbb2ef596771d4f3f
jl6017 <jl6017@columbia.edu>
2021-11-15 11:07:57 -0500

    xcode file

---
+++ opengl1115/opengl1115/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  opengl1115
+//
+//  Created by jiong lin on 11/15/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
